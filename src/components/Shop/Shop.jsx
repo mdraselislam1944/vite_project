ee7267d45a31d4a94191e53db7870621356e7273
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-03-27 22:35:10 +0600

    add localstorage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -11,11 +12,15 @@ useEffect(()=>{
     .then(res=>res.json())
     .then(data=>setProducts(data));
 },[]);
-
+useEffect(()=>{
+    const storeCart=getShoppingCart();
+    console.log(storeCart);
+},[]);
 const handleProduct=(product)=>{
 
         const newCart=[...cart,product];
         setCart(newCart); 
+        addToDb(product.id);
 }
 
     return (
